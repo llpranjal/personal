ee3a7714500c0d84221adc9ab7c021bf3a4688d0
Pranjal <kattelpranjal01@gmail.com>
2024-08-07 17:05:05 -0400

    todo
    kinda confusing but okay

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoCard from "./TodoCard";
+
+export default function TodoList(props) {
+  const { todos, handleDeleteTodo, handleEditTodo } = props;
+
+  return (
+    <ul className="main">
+      {todos.map((todo, todoIndex) => (
+        <TodoCard
+          key={todoIndex}
+          index={todoIndex}
+          handleDeleteTodo={handleDeleteTodo}
+          handleEditTodo={handleEditTodo}
+        >
+          <p>{todo}</p>
+        </TodoCard>
+      ))}
+    </ul>
+  );
+}
